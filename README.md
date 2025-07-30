663d2f1947ced96523b08e772892291b96dab2bf
Sam Ford <sammyryan93@hotmail.co.uk>
2025-07-30 01:26:01 +0100

    Add restaurant info to response

---
+++ README.md
@@ -0,0 +1,3 @@
+***Delectable***
+
+A little Go app for me to gain some knowledge of programming with gen AI and Temporal
--- handlers.go
+++ handlers.go
@@ -11,16 +11,37 @@ import (
 	"google.golang.org/genai"
 )
 
-var PROMPT_TEMPLATE = "Give me the names (only the name, not the description or ingredients) of %d dishes that " +
+var GENAI_MODEL = "gemini-2.5-flash"
+
+var SEARCH_PROMPT_TEMPLATE = "Give me the names (only the name, not the description or ingredients) of %d dishes that " +
 	"use the following ingredients as the base ingredients: %s. Separate the names using the | symbol"
+
+var RESTAURANT_PROMPT_TEMPLATE = `Find a restaurant that serves %s in or near %s.
+	format your response like this, replacing the placeholders with the relevant information.
+	If you can't find any particular field, replace it with "None found":
+	[restaurant name]|[website]|[telephone no.]|[address]|[bool indicating whether takes reservations]`
 
 type SearchRequest struct {
 	Ingredients []string `json: "ingredients"`
 	DishCount   int      `json: "dishCount"`
+	Location    string   `json: "location`
 }
 
 type SearchResponse struct {
-	Dishes []string `json: "dishes"`
+	Dishes []DishWithInfo `json: "dishes"`
+}
+
+type RestaurantInfo struct {
+	Name         string `json: "name"`
+	Website      string `json: "website"`
+	Telephone    string `json: "telephone"`
+	Address      string `json: "address"`
+	Reservations string `json: "reservations"`
+}
+
+type DishWithInfo struct {
+	Name        string `json: "name`
+	WhereToFind RestaurantInfo
 }
 
 func dishSearch(ctx context.Context, aiClient genai.Client) http.HandlerFunc {
@@ -34,18 +55,50 @@ func dishSearch(ctx context.Context, aiClient genai.Client) http.HandlerFunc {
 
 		result, err := aiClient.Models.GenerateContent(
 			ctx,
-			"gemini-2.5-flash",
-			genai.Text(fmt.Sprintf(PROMPT_TEMPLATE, search.DishCount, strings.Join(search.Ingredients, ", "))),
+			GENAI_MODEL,
+			genai.Text(fmt.Sprintf(SEARCH_PROMPT_TEMPLATE, search.DishCount, strings.Join(search.Ingredients, ", "))),
 			nil,
 		)
 		if err != nil {
 			log.Fatal(err)
 		}
 
+		var responseDishes []DishWithInfo
+
+		dishes := strings.Split(result.Text(), "|")
+		for dish := range dishes {
+			restaurant := findRestaurantWithDish(ctx, aiClient, dishes[dish], search.Location)
+			responseDishes = append(responseDishes, DishWithInfo{
+				Name:        dishes[dish],
+				WhereToFind: restaurant,
+			})
+		}
+
 		response := SearchResponse{
-			Dishes: strings.Split(result.Text(), "|"),
+			Dishes: responseDishes,
 		}
 
 		json.NewEncoder(w).Encode(&response)
+	}
+}
+
+func findRestaurantWithDish(ctx context.Context, aiClient genai.Client, dishName string, location string) RestaurantInfo {
+	result, err := aiClient.Models.GenerateContent(
+		ctx,
+		GENAI_MODEL,
+		genai.Text(fmt.Sprintf(RESTAURANT_PROMPT_TEMPLATE, dishName, location)),
+		nil,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resultFields := strings.Split(result.Text(), "|")
+	return RestaurantInfo{
+		Name:         resultFields[0],
+		Website:      resultFields[1],
+		Telephone:    resultFields[2],
+		Address:      resultFields[3],
+		Reservations: resultFields[4],
 	}
 }
